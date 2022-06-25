24ad9d6420af07e2fc502553d1854d5b5dfb98d4
Mert Tecimen <merttecimen@yahoo.com.tr>
2022-06-25 08:26:23 +0300

    Initial Commit

---
+++ SnapKit and GraphQL Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKit and GraphQL Practice
+//
+//  Created by Mert Tecimen on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
