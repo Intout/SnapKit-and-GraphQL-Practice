6ec7e91b48843057aa0c320e586d641f52dd982b
Mert Tecimen <merttecimen@yahoo.com.tr>
2022-06-27 13:26:33 +0300

    Added integration test for RestAPI call and Image downloading.

--- SnapKit and GraphQL PracticeTests/SnapKit_and_GraphQL_PracticeTests.swift
+++ SnapKit and GraphQL PracticeTests/SnapKit_and_GraphQL_PracticeTests.swift
@@ -10,27 +10,41 @@ import XCTest
 
 class SnapKit_and_GraphQL_PracticeTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testDataFetchingFromRestAPI() throws{
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        
+        
+        let expectation = expectation(description: "Fetch data from RestAPI")
+        SnapKit_and_GraphQL_Practice.ApolloQueryHelper.shared.apollo.fetch(query: CharactersQuery()){ result in
+            switch result{
+            case .success(_):
+                XCTAssert(true, "Success!")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTAssert(false, error.localizedDescription)
+                expectation.fulfill()
+            }
         }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    
+    func testDownloadingImage() throws{
+        let url = URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg")!
+        let expectation = expectation(description: "Download image from URL.")
+        
+        Networking.downloadImage(url: url){ result in
+            switch result{
+            case .success(_):
+                XCTAssert(true, "Success!")
+                expectation.fulfill()
+            case .failure(let error):
+                XCTAssert(false, error.localizedDescription)
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5)
+        
     }
 
 }
